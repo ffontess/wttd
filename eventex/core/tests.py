f9b3ee10f612b76df9548ffc3c3e3ea5d0263371
Felipe Fontes <uffontes@gmail.com>
2018-06-22 01:01:39 -0300

    Tela de Inscrição

--- eventex/core/tests.py
+++ eventex/core/tests.py
@@ -3,11 +3,10 @@ from django.test import TestCase
 
 class HomeTest(TestCase):
     def setUp(self):
-        self. response = self.client.get('/')
+        self.response = self.client.get('/')
 
     def test_get(self):
         '''GET / must return code 200'''
-
         self.assertEqual(200, self.response.status_code)
 
     def test_template(self):
